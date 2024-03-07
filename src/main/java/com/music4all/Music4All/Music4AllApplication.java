6cc73cc7813a6f12150222819f9c5b733a47997f
William <william@ampliato.com.br>
2024-03-06 21:42:21 -0300

    clean Main code

--- src/main/java/com/music4all/Music4All/Music4AllApplication.java
+++ src/main/java/com/music4all/Music4All/Music4AllApplication.java
@@ -1,6 +1,5 @@
 package com.music4all.Music4All;
 
-import com.music4all.Music4All.services.implementations.StorageService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -15,18 +14,10 @@ public class Music4AllApplication {
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(Music4AllApplication.class, args);
 		System.out.println("Iniciou - Music4All");
-//		String userDir = System.getProperty("user.dir");
-//		String userHome = System.getProperty("user.home");
-//		System.out.println("path init: " + userDir + "\\src\\main\\resources\\static\\images");
-//		System.out.println("path home: " + userHome);
-
-		StorageService storageService = context.getBean(StorageService.class);
-		System.out.println(storageService.getSongFileNames());
 	}
 
 	@Bean
 	BCryptPasswordEncoder bCryptPasswordEncoder(){
 		return new BCryptPasswordEncoder();
 	}
-	// possivel nome: BandBridge
 }
