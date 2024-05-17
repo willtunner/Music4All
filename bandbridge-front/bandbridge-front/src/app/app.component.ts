c918e2104ee043fb070eb02b093dd51c1fb9d380
willtunner <williampereira21@gmail.com>
2024-05-17 17:02:43 -0300

    install bootstrap 5

--- bandbridge-front/bandbridge-front/src/app/app.component.ts
+++ bandbridge-front/bandbridge-front/src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgbAccordionModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'bandbridge-front';
+
+  status: boolean = false;
+
+  clickEvent(){
+      this.status = !this.status;       
+  }
 }
